d669c45ea678abecc2f563a7af6308d7560d36f3
burkerking72 <ryburke15@gmail.com>
2020-02-01 13:29:49 -0800

    Initial commit

---
+++ app/src/main/java/com/ryanthomasburke/www/inventorymanagementsystem2020/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ryanthomasburke.www.inventorymanagementsystem2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+/*
+This will be the main project space for the Inventory Management System Application
+That I will be developing for Individual Study. It will be in a GitHub Repository.
+ */
